b0868c3e75849591c90953af325a7db3abd1ba7e
Will Sonnex <will@sonnex.name>
2016-11-06 17:10:19 +0000

    Forgot we needed Zeno.hs on the path

--- index.php
+++ index.php
@@ -3,9 +3,10 @@
 <h1>Zeno v0.2.1.0</h1>
 <p>running with a 120 second timeout.</p>
 <form method="POST" action="zeno.php">
-<textarea name="src">
+<textarea rows="20" cols="50" name="src">
 <?= file_get_contents('Example.hs') ?>
 </textarea>
+<br/>
 <input type="submit" value="Prove with Zeno"/>
 </form>
 </body>
--- zeno.php
+++ zeno.php
@@ -1,12 +1,14 @@
 <?php
 $haskell_src = $_REQUEST['src'];
-$file = tmpfile();
+$filename = "hs/" . uniqid() . ".hs";
 
-// Gotta get the file-name for the temp file
-$meta_data = stream_get_meta_data($file);
-$filename = $meta_data['uri'];
+try {
+	$file = fopen($filename, "w");
+	fwrite($file, $haskell_src);
+	fclose($file);
 
-fwrite($file, $haskell_src);
-fclose($file);
-echo shell_exec('timeout 120s - zeno --tryzeno ' . $filename);
+	echo shell_exec("timeout 120s zeno --tryzeno $filename 2>&1");
+} finally {
+	unlink($filename);
+}
 ?>
