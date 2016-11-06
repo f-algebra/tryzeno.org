b1c953a4191e8417927edcf2a08863807843f188
Will Sonnex <will@sonnex.name>
2016-11-06 17:42:33 +0000

    more stylin'

--- index.php
+++ index.php
@@ -1,9 +1,11 @@
 <html>
 <body>
-<h1>Zeno v0.2.1.0</h1>
-<p>running with a 120 second timeout.</p>
+<h2>Zeno v0.2 - An automated theorem prover for Haskell programs.</h2>
+<h3>Developed at Imperial College London by William Sonnex, Sophia Drossopoulou and Susan Eisenbach.</h3>
+<p><a href="http://www.haskell.org/haskellwiki/Zeno">http://www.haskell.org/haskellwiki/Zeno</a></p>
+<p>(running with a 120 second timeout)</p>
 <form method="POST" action="zeno.php">
-<textarea rows="30" cols="64" name="src">
+<textarea rows="30" cols="80" name="src">
 <?= file_get_contents('Example.hs') ?>
 </textarea>
 <br/>
