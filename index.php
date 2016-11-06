58d5d8353cf8a7870582b30527b33664c633f740
Will Sonnex <will@sonnex.name>
2016-11-06 17:20:40 +0000

    stylin

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <h1>Zeno v0.2.1.0</h1>
 <p>running with a 120 second timeout.</p>
 <form method="POST" action="zeno.php">
-<textarea rows="40" cols="100" name="src">
+<textarea rows="30" cols="64" name="src">
 <?= file_get_contents('Example.hs') ?>
 </textarea>
 <br/>
