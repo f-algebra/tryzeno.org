a3224651337d9c0e6962ffe210f9873dfe8e665e
Will Sonnex <will@sonnex.name>
2016-11-06 14:07:24 +0000

    initial draft of zeno.php

---
+++ zeno.php
@@ -0,0 +1,16 @@
+<?php
+$haskell_src = $_POST['src'];
+echo $haskell_src;
+
+$file = tmpfile();
+
+// Gotta get the file-name for the temp file
+$file_meta_data = stream_get_meta_data($file);
+$filename = $meta_data['uri'];
+
+echo $filename;
+
+fwrite($file, $haskell_src);
+fclose($file);
+echo shell_exec('zeno ' . $filename);
+?>
